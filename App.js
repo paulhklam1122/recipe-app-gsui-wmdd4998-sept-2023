15aeb01fe1b2f35d6a6d6cec0f8365ed12884220
Paul Lam <paulhklam1122@gmail.com>
2024-09-16 22:03:06 -0700

    upgraded packages to their latest versions

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
-import { GluestackUIProvider, Text, Box, config } from '@gluestack-ui/themed'
+import { GluestackUIProvider, Text, Box } from '@gluestack-ui/themed'
+import { config } from '@gluestack-ui/config'
 import Header from './src/components/layout/Header'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import AppStack from './src/components/stacks/AppStack'
@@ -7,7 +8,7 @@ import AppStack from './src/components/stacks/AppStack'
 const App = () => {
   return (
     <SafeAreaProvider>
-      <GluestackUIProvider config={config.theme}>
+      <GluestackUIProvider config={config}>
         {/* <Header /> */}
         <AppStack />
         <StatusBar bg='#2c3e50' />
